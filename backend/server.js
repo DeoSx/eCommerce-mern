af46d0a3c0c28c89bea65f775df28752b228942d
DeoSx <deosxz123@gmail.com>
2020-10-20 22:40:10 +0600

    feat: add middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+
 const productRoutes = require('./routes/productRoutes')
 
 dotenv.config()
@@ -13,6 +15,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
