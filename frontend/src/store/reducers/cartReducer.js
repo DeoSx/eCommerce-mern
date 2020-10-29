ae826c93355617e075242832460eb2986c5abd71
DeoSx <deosxz123@gmail.com>
2020-10-29 22:34:18 +0600

    feat(cart): actions, constants to add

---
+++ frontend/src/store/reducers/cartReducer.js
@@ -0,0 +1,33 @@
+import { CART_ADD_ITEM } from '../constants'
+
+const initialState = {
+  cartItems: []
+}
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload
+
+      const existItem = state.cartItems.find((x) => x.product === item.product)
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          )
+        }
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item]
+        }
+      }
+
+    default:
+      return state
+  }
+}
+
+export default cartReducer
