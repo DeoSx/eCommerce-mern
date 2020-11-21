cad797d263e6b722527a7a46df70fc9d258ddc26
DeoSx <deosxz123@gmail.com>
2020-11-21 22:49:22 +0600

    feat: add register with mongoose pre hooks

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -31,4 +31,13 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 module.exports = model('User', UserSchema)
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express')
-const { authLogin, getUserProfile } = require('../controllers/userController')
+const {
+  authLogin,
+  getUserProfile,
+  registerUser
+} = require('../controllers/userController')
 
 const authProtect = require('../middleware/authMiddleware')
 
 const router = Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authLogin)
 router.route('/profile').get(authProtect, getUserProfile)
 
